5b616e585d8c613004f37ea7a6560678be8b8b4d
Chibunkem Asuzu <c.masuzu@gmail>
2026-03-13 04:56:33 -0400

    v5

--- Components/navbar.py
+++ Components/navbar.py
@@ -2,10 +2,15 @@ from nicegui import ui
 
 
 def navbar():
-    with ui.row().classes('w-full justify-between items-center bg-gray-200 p-3'):
-        ui.label('My App').classes('text-xl font-bold')
+    with ui.row().classes(
+            'w-full justify-between items-center bg-gray-200 p-3'
+    ):
+        # Left side (logo + name)
+        with ui.row().classes('items-center gap-2'):
+            ui.image('/static/Logo2_Transparent.png').classes('w-20 h-20')
+            # ui.label('VeriDAI').classes('text-xl font-bold')
 
+        # Right side
         with ui.row():
-            ui.button('Home', on_click=lambda: ui.navigate.to('/'))
-            ui.button('Dashboard', on_click=lambda: ui.navigate.to('/dashboard'))
-            ui.button('Login', on_click=lambda: ui.navigate.to('/login'))
+            #ui.button('Home', on_click=lambda: ui.navigate.to('/'))
+            ui.button('Login', on_click=lambda: ui.navigate.to('/'))
